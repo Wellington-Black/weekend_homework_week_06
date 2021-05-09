fc28478cce2159a462db172abf566c97b904fb83
Wellington-Black <well1482@gmail.com>
2021-05-09 19:00:48 +0100

    Added Delete All Button

--- js/app.js
+++ js/app.js
@@ -1,18 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('#form');
     form.addEventListener('submit', handleFormSubmit);
+
+    const deleteButton = document.querySelector('#delete-all');
+    deleteButton.addEventListener('click', handleDeleteAll);
+
 });
 
 const handleFormSubmit = function (event) {
     event.preventDefault();
-    console.log(event);
+    console.log(event.target);
 
-    // const newAnimeName = document.createElement('li');
-    // newAnimeName.textContent = `${event.target.name.value}`;
-    // // newAnimeName.classList.add(`${event.target.name.value}`);
+    const newAnimeEntry = createAnimeEntry(event.target);
+    const animeList = document.querySelector('#anime-list');
+    animeList.appendChild(newAnimeEntry);
 
-    // const list = document.querySelector('ul');
-    // list.appendChild(newAnimeName);
+    // event.target.reset();
+    
+}
 
+const createAnimeEntry = function (form) {
 
+    const newAnimeEntry = document.createElement('li');
+
+    const title = document.createElement('h2');
+    title.textContent = form.title.value;
+    newAnimeEntry.appendChild(title);
+
+    const genre = document.createElement('p');
+    genre.textContent = form.genre.value;
+    newAnimeEntry.appendChild(genre);
+
+    const year = document.createElement('p');
+    year.textContent = form.year.value;
+    newAnimeEntry.appendChild(year);
+
+    return newAnimeEntry;
+}
+
+const handleDeleteAll = function (event) {
+    const animeList = document.querySelector('#anime-list')
+    animeList.innerHTML = '';
+    // console.log(animeList)
+    // animeList.parentNode.removeChild(animeList);
+    // animeList.remove();
 }
